84a792e9ac0035f0b4a70abc68d8710b77902487
antoineleno <johndoe@example.com>
2024-01-31 03:44:11 +0800

    printf

--- _print_unsigned_int.c
+++ _print_unsigned_int.c
@@ -10,8 +10,29 @@
 int prt_u_i(char  *buffer, va_list list_of_argument)
 {
 	unsigned int number = va_arg(list_of_argument, unsigned int);
+	int length = 0, count = 0, i;
+	char temporarybuffer;
 
-	sprintf(buffer, "%u", number);
-	write(1, buffer, 20);
-	return (_strlen(buffer));
+
+	do {
+		buffer[length++] = number % 10 + '0';
+		number = number / 10;
+
+	} while (number > 0);
+
+	if (length > 12)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temporarybuffer = buffer[i];
+		buffer[i] = buffer[length - i - 1];
+		buffer[length - i - 1] = temporarybuffer;
+	}
+
+	buffer[length++] = '\0';
+	count += write(1, buffer, length);
+	return (count);
 }
