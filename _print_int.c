63a4804a52a243138c8718f5c101ec59743e6201
antoineleno <johndoe@example.com>
2024-01-30 13:29:55 +0800

    printf

--- _print_int.c
+++ _print_int.c
@@ -2,44 +2,48 @@
 
 /**
  * prt_i - Function to print an integer.
- * @buffer: Buffer to store the character printed.
  * @list_of_argument: Argument to pass while printing.
+ * @buffer: Buffer to store the character printed.
  * Return: Always the number of digits printed
- */
+*/
 int prt_i(char *buffer, va_list list_of_argument)
 {
-    int n = va_arg(list_of_argument, int);
-    int i = 0;
+	int n = va_arg(list_of_argument, int);
+	int number, remainder, digit;
+	int i = 1;
+	int exp = 1;
 
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-        i++;
-    }
+	remainder = n % 10
 
-    if (n == 0) {
-        _putchar('0');
-        i++;
-    } else {
-        int exp = 1;
-        int number = n;
+	n = n / 10;
+	number = n;
 
-        while (number / 10 != 0) {
-            exp *= 10;
-            number /= 10;
-        }
-
-        number = n;
-
-        while (exp > 0) {
-            int digit = number / exp;
-            _putchar(digit + '0');
-            number %= exp;
-            exp /= 10;
-            i++;
-        }
-    }
-
-    buffer[0] = '\0';
-    return i;
+	if (remainder < 0)
+	{
+		_putchar('-');
+		number = -number;
+		n = -n;
+		remainder = -last;
+		i++;
+	}
+	if (number > 0)
+	{
+		while (number / 10 != 0)
+		{
+			exp = exp * 10;
+			number = number / 10;
+		}
+		number = n;
+		while (exp > 0)
+		{
+			digit = number / exp;
+			_putchar(digit + '0');
+			number = number - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	buffer[0] = '\0';
+	_putchar(remainder + '0');
+	return (i);
 }
