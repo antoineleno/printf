5857a5c2692676980156d897a1b0ba2374ddf843
antoineleno <johndoe@example.com>
2024-01-30 15:26:17 +0800

    printf

--- _print_int.c
+++ _print_int.c
@@ -1,32 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>  // Include the necessary header for malloc
 
 /**
  * prt_i - Function to print an integer.
  * @list_of_argument: Argument to pass while printing.
  * @buffer: Buffer to store the character printed.
  * Return: Always the number of digits printed
- */
+*/
 int prt_i(char *buffer, va_list list_of_argument)
 {
-    int n = va_arg(list_of_argument, int);
-    buffer = (char *)malloc(12 * sizeof(char));
+	int n = va_arg(list_of_argument, int);
 
-    if (buffer == NULL) {
-        return -1;
-    }
-
-    int length = sprintf(buffer, "%d", n);
-
-    if (length < 0) {
-        free(buffer);
-        return -1;
-    }
-
-    int written = write(1, buffer, length);
-
-    free(buffer);
-
-    return written;
+	int length = sprintf(buffer, "%d", n);
+	if (length < 0)
+	{
+		return (-1);
+	}
+	return (write(1, buffer, length));
 }
