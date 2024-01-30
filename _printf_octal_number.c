4fb3527f5dc9241ca3ceb8794d37776bc6a69f29
antoineleno <johndoe@example.com>
2024-01-31 04:35:12 +0800

    printf

--- _printf_octal_number.c
+++ _printf_octal_number.c
@@ -10,27 +10,9 @@
 int prt_o(char *buffer, va_list list_of_argument)
 {
 	unsigned int number = va_arg(list_of_argument, int);
-	unsigned int length = 0;
-	unsigned int count = 0;
-	unsigned int k;
-	char tmp;
 
-	while (number > 0)
-	{
-		buffer[length++] = number % 8 + '0';
-		number = number / 8;
-	}
+	buffer = (char *)malloc(20 * sizeof(char));
 
-	if (length == 0)
-	{
-		return (count += _putchar('0'));
-	}
-
-	for (k = 0; k < length / 2; k++)
-	{
-		tmp = buffer[k];
-		buffer[k] = buffer[length - k - 1];
-		buffer[length - k - 1] = tmp;
-	}
-	return (write(1, buffer, _strlen(buffer)));
+	sprintf(buffer, "%o", number);
+	return (write(1, buffer, 20));
 }
