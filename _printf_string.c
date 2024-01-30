66a95324e6fdcf3540fa6b187875775355e3fe26
antoineleno <johndoe@example.com>
2024-01-30 11:16:56 +0800

    printf

--- _printf_string.c
+++ _printf_string.c
@@ -9,20 +9,22 @@
 
 int prt_s(char *buffer, va_list list_of_argument)
 {
-	char *str = va_arg(list_of_argument, char *);
-	int count = 0;
-	int length = _strlen(str);
+	char *str;
+	int count = 0, length;
+	
+	str = va_arg(list_of_argument, char *);
+
+	length = _strlen(str);
 
 	if (length >= buffersize)
 	{
-		count = write(1, str, length);
+		count += write(1, str, length);
 	}
 
 	else
 	{
 		_strncpy(buffer, str, buffersize - 1);
-		buffer[buffersize - 1] = '\0';
-		count =  write(1, buffer, length);
+		count +=  write(1, buffer, length);
 	}
 
 	return (count);
