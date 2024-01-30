20c917ac1dba7bad96fcd1f5563334b4bb89b1f8
antoineleno <johndoe@example.com>
2024-01-31 02:40:13 +0800

    printf

--- _print_int.c
+++ _print_int.c
@@ -10,12 +10,19 @@
 int prt_i(char *buffer, va_list list_of_argument)
 {
 	int n = va_arg(list_of_argument, int);
-
+	int i;
 	int length = sprintf(buffer, "%d", n);
 
 	if (length < 0)
 	{
 		return (-1);
 	}
-	return (write(1, buffer, length));
+	else
+	{
+		for (i = 0; i < length; i++)
+		{
+			_putchar(buffer[i]);
+		}
+	}
+	return (length);
 }
