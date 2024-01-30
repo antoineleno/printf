ec8170e5c710b2b1572a634672448a0214afc29a
antoineleno <johndoe@example.com>
2024-01-31 05:59:39 +0800

    printf

--- _printf_hexadecimal_X.c
+++ _printf_hexadecimal_X.c
@@ -11,7 +11,7 @@ int prt_hx_X(char *buffer, va_list list_of_argument)
 {
 	unsigned int hexadecimal_number = va_arg(list_of_argument, unsigned int);
 
-	int length = sprintf(buffer, "%x", hexadecimal_number);
+	int length = sprintf(buffer, "%X", hexadecimal_number);
 	return (write(1, buffer, length));
 }
 
