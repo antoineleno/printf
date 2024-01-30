4c7e983662539b291f3ad51a3274e39d58b7106f
antoineleno <johndoe@example.com>
2024-01-31 03:37:08 +0800

    printf

--- short.c
+++ short.c
@@ -52,7 +52,7 @@ short int _print_h_o(short int number)
 {
 	char buffer[16];
 	sprintf(buffer, "%hd", number);
-	write(1, buffer, 20);
+	write(1, buffer, 16);
 	return (16);
 }
 
@@ -64,42 +64,10 @@ short int _print_h_o(short int number)
 
 short int print_h_x(short int hexadecimal_number)
 {
-	char buffer[20];
-	short int remainder = 0;
-	short int length = 0, m;
-	char tmp;
-
-	if (hexadecimal_number == 0)
-	{
-		buffer[length++] = '0';
-	}
-
-	while (hexadecimal_number > 0)
-	{
-		remainder = hexadecimal_number % 16;
-		if (remainder < 10)
-		{
-			buffer[length++] = remainder + '0';
-		}
-
-		else
-		{
-			remainder = remainder - 10 + 'a';
-			buffer[length++] = remainder;
-		}
-
-		hexadecimal_number = hexadecimal_number / 16;
-	}
-
-	for (m = 0; m < length / 2; m++)
-	{
-		tmp = buffer[m];
-		buffer[m] = buffer[length - m - 1];
-		buffer[length - m - 1] = tmp;
-	}
-
-	buffer[length++] = '\0';
-	return (write(1, buffer, _strlen(buffer)));
+	char buffer[16];
+	sprintf(buffer, "%hd", hexadecimal_number);
+	write(1, buffer, 16);
+	return (16);
 }
 
 /**
@@ -110,42 +78,10 @@ short int print_h_x(short int hexadecimal_number)
 
 short int _printf_h_X(short int hexadecimal_number)
 {
-	char buffer[20];
-	short int remainder = 0;
-	short int length = 0, m;
-	char tmp;
-
-	if (hexadecimal_number == 0)
-	{
-		buffer[length++] = '0';
-	}
-
-	while (hexadecimal_number > 0)
-	{
-		remainder = hexadecimal_number % 16;
-		if (remainder < 10)
-		{
-			buffer[length++] = remainder + '0';
-		}
-
-		else
-		{
-			remainder = remainder - 10 + 'A';
-			buffer[length++] = remainder;
-		}
-
-		hexadecimal_number = hexadecimal_number / 16;
-	}
-
-	for (m = 0; m < length / 2; m++)
-	{
-		tmp = buffer[m];
-		buffer[m] = buffer[length - m - 1];
-		buffer[length - m - 1] = tmp;
-	}
-
-	buffer[length++] = '\0';
-	return (write(1, buffer, _strlen(buffer)));
+	char buffer[16];
+	sprintf(buffer, "%hd", hexadecimal_number);
+	write(1, buffer, 16);
+	return (16);
 }
 
 /**
@@ -156,30 +92,8 @@ short int _printf_h_X(short int hexadecimal_number)
 
 short int print_h_u_i(short int number)
 {
-	char buffer[20];
-	unsigned short int length = 0, count = 0, i;
-	char temporarybuffer;
-
-
-	do {
-		buffer[length++] = number % 10 + '0';
-		number = number / 10;
-
-	} while (number > 0);
-
-	if (length > 12)
-	{
-		return (-1);
-	}
-
-	for (i = 0; i < length / 2; i++)
-	{
-		temporarybuffer = buffer[i];
-		buffer[i] = buffer[length - i - 1];
-		buffer[length - i - 1] = temporarybuffer;
-	}
-
-	buffer[length++] = '\0';
-	count += write(1, buffer, length);
-	return (count);
+	char buffer[16];
+	sprintf(buffer, "%hd", number);
+	write(1, buffer, 16);
+	return (16);
 }
